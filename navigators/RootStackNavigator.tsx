a04f37dcb74b113200ed3ae48de1efb8bf59a9e5
Davod <davodnikoyii@gmail.com>
2024-09-27 09:45:54 +0200

    RootStackNavigator has been created with two screens

--- navigators/RootStackNavigator.tsx
+++ navigators/RootStackNavigator.tsx
@@ -1,9 +1,21 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import DetailsScreen from "../screens/DetailsScreen";
+import HomeScreen from "../screens/HomeScreen";
 
 
 type RootStackParamList = {
     Home: undefined;
-    Detail: { id: number };
+    Details: { id: number };
 }
 
 const RootStack = createNativeStackNavigator<RootStackParamList>();
+
+export default function RootStackNavigator(){
+
+    return(
+        <RootStack.Navigator>
+            <RootStack.Screen name="Home" component={HomeScreen} />
+            <RootStack.Screen name="Details" component={DetailsScreen} />
+        </RootStack.Navigator>
+    );
+} 
